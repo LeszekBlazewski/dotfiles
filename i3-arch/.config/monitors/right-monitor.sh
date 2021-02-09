e6ebf9a11914724955ec3630376045bb1c696d1c
LeszekBlazewski <blazewski.leszek@gmail.com>
2021-02-09 11:36:14 +0100

    Add right monitor script

---
+++ i3-arch/.config/monitors/right-monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# executed from i3 config on i3 restart mod+shift+r
+# when HDMI2 connected setup for right monitor
+# when HDMI2 disconnected setup only for main display (mod+shift+r)
+
+if xrandr | grep 'HDMI-2 connected' ; then
+    xrandr --output eDP1 --auto --output HDMI-2 --auto --right-of eDP-1
+else
+     xrandr --output eDP-1 --auto --output HDMI-2 --off
+fi
