cfe21ec766a24d77a5402b87dc83648f4626637f
LeszekBlazewski <blazewski.leszek@gmail.com>
2021-03-19 14:20:46 +0100

    Polybar on all monitors

--- i3-arch/.config/polybar/launch.sh
+++ i3-arch/.config/polybar/launch.sh
@@ -5,7 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main  -r  &
-#echo "Bars launched..."
+# multiple monitors bars
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar main -r &
+  done
+else
+  polybar main -r &
+fi
