2ade7a711c74b1a71309b6ae7b47c7700268a589
LeszekBlazewski <blazewski.leszek@gmail.com>
2021-06-27 16:22:59 +0200

    dunst config and script changes

--- install-i3.sh
+++ install-i3.sh
@@ -28,10 +28,27 @@ cd dotfiles/i3-purple
 # install all of the packages from i3-purple setup
 yay -S --needed --noconfirm - < pkglist.txt
 
-# enable lightdm and gnome-keyring in it
+# enable lightdm
 systemctl enable lightdm
-sudo sed -i 's/-auth/auth/' /etc/pam.d/lightdm
-sudo sed -i 's/-session/session/' /etc/pam.d/lightdm
+
+# auto unlock gnome-keyring
+for file in "/etc/pam.d/lightdm" "/etc/pam.d/lightdm-autologin"
+do
+    sudo sed -i 's/-auth/auth/' $file
+    sudo sed -i 's/-session/session/' $file
+done
+
+sudo sed -i '3 a auth	  optional	pam_gnome_keyring.so' /etc/pam.d/login
+sudo sed -i '$ a session	  optional	pam_gnome_keyring.so auto_start' /etc/pam.d/login
+
+# copy and enable lightdm theme
+sudo cp -a themes/.themes/Dracula /usr/share/themes
+sudo cp -a icons/.icons/Dracula /usr/share/icons
+sudo cp -a wallpaper/.config/wallpaper/wallpaper.jpg /usr/share/wallpapers
+sudo sed -i 's|#background=|background=/usr/share/wallpapers/wallpaper.jpg|' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's|#theme-name=|theme-name=Dracula|' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's|#icon-theme-name=|icon-theme-name=Dracula|' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/Inherits=Adwaita/Inherits=Breeze_Purple/' /usr/share/icons/default
 
 # enable bluetooth deamon
 systemctl enable bluetooth.service
@@ -44,14 +61,6 @@ cp -a grub.config/grub/themes/liquid-amethyst /boot/grub/themes/liquid-amethyst
 sudo sed -i 's|#GRUB_THEME=|GRUB_THEME="/boot/grub/themes/liquid-amethyst/theme.txt"|' /etc/default/grub
 sudo sed -i 's/#GRUB_GFXMODE=/GRUB_GFXMODE=1920x1080/'   /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-
-# copy and enable lightdm theme
-sudo cp -a themes/.themes/Dracula /usr/share/themes
-sudo cp -a icons/.icons/Dracula /usr/share/icons
-sudo cp -a wallpaper/.config/wallpaper/wallpaper.jpg /usr/share/wallpapers
-sudo sed -i 's|#background=|background=/usr/share/wallpapers/wallpaper.jpg|' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's|#theme-name=|theme-name=Dracula|' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's|#icon-theme-name=|icon-theme-name=Dracula|' /etc/lightdm/lightdm-gtk-greeter.conf
 
 # open terminal in nautilius
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
